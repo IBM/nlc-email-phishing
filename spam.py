a0ad906e3528410824e5882d31649d6b457d9dfa
Scott D'Angelo <scott.dangelo@gmail.com>
2018-08-22 10:43:05 -0600

    Fix to use .env file. (#4)
    Add logic to parse .env file first, then VCAP.
    Add .env to .gitignore
    Update packages.
    Remove hard-coded classifier_id.

--- spam.py
+++ spam.py
@@ -4,23 +4,37 @@ __author__ = 'dimascio'
 import requests
 import json
 
-# Replace YOUR_CLASSIFIER_ID, YOUR_CLASSIFIER_USERNAME, and YOUR_CLASSIFIER_PASSWORD
-# with the information provided in your classifier's credentials object
+# Replace NATURAL_LANGUAGE_CLASSIFIER_USERNAME and
+# NATURAL_LANGUAGE_CLASSIFIER_PASSWORD with the information provided in
+# your classifier's credentials object
+# Use the NLC_CLASSIFIER_ID obtained after training the model
+NATURAL_LANGUAGE_CLASSIFIER_USERNAME = ""
+NATURAL_LANGUAGE_CLASSIFIER_PASSWORD = ""
+NLC_CLASSIFIER_ID = ""
+GATEWAY_URL = "https://gateway.watsonplatform.net/"
+NLC_PATH = "natural-language-classifier/api/v1/classifiers/"
+
+
 def classify(s):
-    return requests.post("https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/e9fa7bx367-nlc-825/classify",
-                      json.dumps({'text':s}),
-                      auth=("YOUR_CLASSIFIER_USERNAME", "YOUR_CLASSIFIER_PASSWORD"),
-                      headers={'Content-Type': 'application/json'})
+    return requests.post(GATEWAY_URL + NLC_PATH + NLC_CLASSIFIER_ID +
+                         "/classify",
+                         json.dumps({'text': s}),
+                         auth=(NATURAL_LANGUAGE_CLASSIFIER_USERNAME,
+                               NATURAL_LANGUAGE_CLASSIFIER_PASSWORD),
+                         headers={'Content-Type': 'application/json'})
+
 
 # Read test data into test array
-
 test = []
-with open('data/Email-testingdata.json') as testData:
-	test=json.load(testData);
-	# Classify each test observation and store its prediction and label
-	predictionsAndLabels = map(lambda o:  (classify(o['Text']).json(), o['Class'][0] ), test)
 
-	# Calculate the classifier's accuracy by comparing:
-	# Number of correct predictions / Number of test observations
-	accuracy = 1.0 * len(filter(lambda x: x[0]['top_class'] == x[1], predictionsAndLabels)) / len(test)
-	print "accuracy: %s" % accuracy
+with open('data/Email-testingdata.json') as testData:
+    test = json.load(testData)
+    # Classify each test observation and store its prediction and label
+    predictionsAndLabels = map(lambda o:  (classify(o['Text']).json(),
+                               o['Class'][0]), test)
+
+# Calculate the classifier's accuracy by comparing:
+# Number of correct predictions / Number of test observations
+    accuracy = 1.0 * len(filter(lambda x: x[0]['top_class'] == x[1],
+                         predictionsAndLabels)) / len(test)
+print "accuracy: %s" % accuracy
--- web/routes/classify.js
+++ web/routes/classify.js
@@ -1,39 +1,76 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var NLC = require('watson-developer-cloud/natural-language-classifier/v1');
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-var classifierId = process.env.CLASSIFIER_ID || 'e9fa7bx367-nlc-825';
+'use strict';
+
+require('dotenv').config({
+  silent: true,
+});
+
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const NLC = require('watson-developer-cloud/natural-language-classifier/v1');
+const vcapServices = require('vcap_services');
+
+var classifierId = process.env.NLC_CLASSIFIER
+
+var  creds = creds()
+if (creds == null){
+  console.log("Failure to create creds")
+}
+
+console.log("classifierId:")
+console.log(classifierId)
+
 router.post('/', function(req, res, next) {
   classify(req, res);
 });
 
 function classify(req, res) {
-  var creds = creds();
+  console.log("Classifying:")
   console.log(req.body);
+
   var nlc = new NLC(creds);
-  console.log('here');
+
   nlc.classify({
     text: req.body.text,
-    classifier_id: classifierId,
+    classifier_id: classifierId
   }, function(err,response){
     if (err) {
       console.log(err)
-      res.status(500).send(err);
     } else {
       res.json(response)
     }
   })
+}
 
-  function creds() {
-    // Extract Bluemix creds for Watson NLC service
-    var VCAP = process.env.VCAP_SERVICES
+function creds() {
+  // Preference is .env file
+  if (process.env.NATURAL_LANGUAGE_CLASSIFIER_USERNAME) {
+      var uname = process.env.NATURAL_LANGUAGE_CLASSIFIER_USERNAME
+      var pword = process.env.NATURAL_LANGUAGE_CLASSIFIER_PASSWORD
+  return {username: uname, password: pword}
+
+  } else {
+  // Extract Bluemix creds for Watson nlc service
+  var VCAP = process.env.VCAP_SERVICES
     if (VCAP) {
       return (JSON.parse(VCAP)).natural_language_classifier[0].credentials;
-    }
-    else {
-     
-    }
+   }
   }
 }
 
